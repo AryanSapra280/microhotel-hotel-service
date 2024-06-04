b4e713b1c6a40ff29e6ed0c733b021549e6f8d6b
Aryan Sapra <aryansaprajiit@gmail.com>
2024-06-04 19:39:04 +0530

    eureka client setup

--- src/main/java/com/hotelservice/hotelService/entities/Hotel.java
+++ src/main/java/com/hotelservice/hotelService/entities/Hotel.java
@@ -1,6 +1,5 @@
 package com.hotelservice.hotelService.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
